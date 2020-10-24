0ae03165fdd4f7c11dbb711ba1d8a12215014bf2
Raul <rchfe23@gmail.com>
2020-10-24 19:47:01 -0400

    Add script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+import os
+
+data_dp = os.path.expanduser("~/Data/book-analysis")
+epub_fp = os.path.join(data_dp, "The Governance of China by Xi Jinping.epub")
+pdf_fp = os.path.join(data_dp, "The Governance of China by Xi Jinping.pdf")
+
+
+if __name__ == '__main__':
+    # epub to text
+    # pdf to text
+    pass
