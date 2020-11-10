b6fb447b2f7fb89d63d3758cec8546c7f8892080
Raul <rchfe23@gmail.com>
2020-11-09 23:47:59 -0500

    Add default_values.py

---
+++ book_analysis/default_values.py
@@ -0,0 +1,23 @@
+CACHE = {
+    'ENABLE_CACHE': True,
+    'DATA_TO_CACHE': {
+        'cache_pages': True,
+        'cache_lexicon': False,
+        'cache_word_counts': False
+    }
+}
+
+TOKENIZER = {
+    'CATEGORY': 'TreebankWordTokenizer',
+    'PREPROCESSING': {
+        'lower_text': True
+    },
+    'FILTERS': {
+        'remove_punctuations': True,
+        'remove_stopwords': True
+    },
+    'SAVE_TOKENS': {
+        'per_book': False,
+        'per_page': True
+    }
+}
