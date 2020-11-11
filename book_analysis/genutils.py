aaad405c1765772ad6c6e3e306228bd4510b18e4
Raul <rchfe23@gmail.com>
2020-11-11 15:10:20 -0500

    genutils: add Cache

--- book_analysis/genutils.py
+++ book_analysis/genutils.py
@@ -1,20 +1,40 @@
 import os
 # Third-party
 import dill
-# Own
+# Custom
 import conf
 
 
 def load_pickle(filepath):
     # TODO: raise IOError
     with open(filepath, 'rb') as f:
+        # TODO: log waiting msg for loading
+        print("\nLoading pickle file ...")
         return dill.load(f)
 
 
+# TODO: add overwrite parameter and check if file already exists
 def save_pickle(obj, filepath, protocal=dill.HIGHEST_PROTOCOL):
     # TODO: raise IOError
     with open(filepath, 'wb') as f:
+        # TODO: log waiting msg for dumping
+        print("\nSaving pickle file ...")
         dill.dump(obj, f, protocol=protocal)
+
+
+class Cache:
+    def __init__(self):
+        self.cache = {}
+
+    def reset_cache(self):
+        self.cache = {}
+
+    # TODO: specify that data should be a dict or set (with update function)
+    def update_cache(self, data, data_type):
+        if self.cache.get(data_type):
+            self.cache[data_type].update(data)
+        else:
+            self.cache[data_type] = data
 
 
 class Config:
@@ -25,7 +45,8 @@ class Config:
         settings = {}
         for opt_name, opt_value in conf.__dict__.items():
             if not opt_name.startswith('__') and not opt_name.endswith('__'):
-                if opt_name.endswith('filepath'):
+                # TODO: add epub_filepath in list
+                if opt_name in ['pdf_filepath']:
                     data_dirpath = os.path.expanduser(conf.data_dirpath)
                     ebook_filepath = os.path.expanduser(opt_value)
                     if os.path.exists(ebook_filepath):
