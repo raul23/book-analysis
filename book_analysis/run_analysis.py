3602728d20c7049d2bbb84148f76de06da635b3e
Raul <rchfe23@gmail.com>
2020-11-11 14:00:12 -0500

    Add run_analysis.py

---
+++ book_analysis/run_analysis.py
@@ -0,0 +1,29 @@
+from book import PDFBook
+from genutils import config, load_pickle, save_pickle
+
+import ipdb
+
+
+if __name__ == '__main__':
+    # ipdb.set_trace()
+    book = PDFBook(
+        pdf_filepath=config.settings.get('pdf_filepath'),
+        book_title=config.settings.get('book_title'),
+        tokenizer_cfg=config.settings.get('TOKENIZER'),
+        cache_cfg=config.settings.get('CACHE')
+    )
+    report = book.analyze(
+        **config.settings.get('include_book_parts'),
+        pages=config.settings.get('pages'),
+        shuffle_tokens=config.settings.get('shuffle_tokens'),
+        random_seed=config.settings.get('random_seed')
+    )
+    ipdb.set_trace()
+    if config.settings.get('save_pickle'):
+        save_pickle(book, config.settings.get('pickle_filepath'))
+    if config.settings.get('save_report'):
+        book.save_report(config.settings.get('report_filepath'))
+    book.close()
+    ipdb.set_trace()
+    book = load_pickle(config.settings.get('pickle_filepath'))
+    book.close()
