8829962d516c79eb0068b5134b56682a5284dd5a
Raul <rchfe23@gmail.com>
2020-11-12 11:20:50 -0500

    run_analysis: load pickle file and analyze book with an updated tokenizer (diff from loaded one)

--- book_analysis/run_analysis.py
+++ book_analysis/run_analysis.py
@@ -3,9 +3,24 @@ from genutils import config, load_pickle, save_pickle
 
 import ipdb
 
+LOAD = False
+
 
 if __name__ == '__main__':
-    # ipdb.set_trace()
+    if LOAD:
+        book = load_pickle(config.settings.get('pickle_filepath'))
+        report = book.analyze(pages=['28-50'])
+
+        ipdb.set_trace()
+
+        book.update_tokenizer(config.settings.get('TOKENIZER'))
+        report = book.analyze(
+            **config.settings.get('include_book_parts'),
+            pages=config.settings.get('pages'),
+            shuffle_tokens=config.settings.get('shuffle_tokens'),
+            random_seed=config.settings.get('random_seed')
+        )
+
     book = PDFBook(
         pdf_filepath=config.settings.get('pdf_filepath'),
         book_title=config.settings.get('book_title'),
@@ -24,6 +39,6 @@ if __name__ == '__main__':
         book.save_report(
             **config.settings.get('filter_words_in_report'),
             filepath=config.settings.get('report_filepath'))
-    book.close()
-    book = load_pickle(config.settings.get('pickle_filepath'))
+    ipdb.set_trace()
+    # TODO: necessary?
     book.close()
