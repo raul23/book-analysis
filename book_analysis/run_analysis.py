80c6fac1439ffbeb112ad2fe6e6348e269b0129d
Raul <rchfe23@gmail.com>
2020-11-13 14:39:24 -0500

    run_analysis: add more testing methods

--- book_analysis/run_analysis.py
+++ book_analysis/run_analysis.py
@@ -1,30 +1,32 @@
-from book import PDFBook
-from genutils import config, load_pickle, save_pickle
+from governance_of_china import GovernancePDFBook
+from genutils import config, load_pdf_book, save_pickle
 
 import ipdb
 
 # For debugging loading pickle files
-LOAD = False
+LOAD = True
 
 
 if __name__ == '__main__':
     # For debugging: testing loading a pickle instance of PDFBook
     if LOAD:
-        book = load_pickle(config.settings.get('pickle_filepath'))
+        ipdb.set_trace()
+        book = load_pdf_book(config.settings.get('pickle_filepath'))
         report = book.analyze(pages=['28-50'])
 
         ipdb.set_trace()
 
-        book.update_tokenizer(config.settings.get('TOKENIZER'))
         report = book.analyze(
             **config.settings.get('include_book_parts'),
-            pages=config.settings.get('pages'),
+            pages=['45', '50-55'],
             shuffle_tokens=config.settings.get('shuffle_tokens'),
             random_seed=config.settings.get('random_seed')
         )
 
+        ipdb.set_trace()
+
     # Instantiate PDFBook
-    book = PDFBook(
+    book = GovernancePDFBook(
         pdf_filepath=config.settings.get('pdf_filepath'),
         book_title=config.settings.get('book_title'),
         tokenizer_cfg=config.settings.get('TOKENIZER'),
@@ -35,6 +37,37 @@ if __name__ == '__main__':
     report = book.analyze(
         **config.settings.get('include_book_parts'),
         pages=config.settings.get('pages'),
+        shuffle_tokens=config.settings.get('shuffle_tokens'),
+        random_seed=config.settings.get('random_seed')
+    )
+
+    ipdb.set_trace()
+
+    book.enable_cache = False
+    book.tokenizer_cfg['PREPROCESSING']['lower_text'] = False
+    book.analyze(
+        include_appendix=False,
+        pages=['30-31'],
+        shuffle_tokens=config.settings.get('shuffle_tokens'),
+        random_seed=config.settings.get('random_seed')
+    )
+
+    ipdb.set_trace()
+
+    book.enable_cache = True
+    book.analyze(
+        **config.settings.get('include_book_parts'),
+        pages=['35-40'],
+        shuffle_tokens=config.settings.get('shuffle_tokens'),
+        random_seed=config.settings.get('random_seed')
+    )
+
+    ipdb.set_trace()
+
+    book.enable_cache = True
+    book.analyze(
+        **config.settings.get('include_book_parts'),
+        pages=['35-40'],
         shuffle_tokens=config.settings.get('shuffle_tokens'),
         random_seed=config.settings.get('random_seed')
     )
